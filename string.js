2537b99d2f9adbb11a92f45598060c3256ebf4b8
Pedro Mariano Cereghetti <pedrocerega@gmail.com>
2025-04-28 17:11:24 -0300

    strings

---
+++ string.js
@@ -0,0 +1,22 @@
+
+// 1
+
+let saludo = "Buenos dias"
+const nombre = "Pedro"
+console.log(`${saludo} ${nombre}`)
+
+// 2
+
+console.log(nombre.length)
+
+// 3
+
+console.log(nombre.charAt(0), nombre.charAt(4))
+
+//4 
+
+console.log(nombre.toLowerCase(), nombre.toUpperCase())
+
+// 5
+
+console.log(nombre + saludo)
