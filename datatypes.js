d2ae0a3af7848499a4a140bb013529eeac2c94fd
Pedro Mariano Cereghetti <pedrocerega@gmail.com>
2025-04-28 15:53:21 -0300

    basics

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+//hola
+{/*holajeje*/}
+/*
+holajeje
+*/
+
+const myName = "name"
+let number = 1
+const caca = true
+let bigNumber = BigInt(312312312312312313213122342342342342343213)
+let noDefined = undefined
+const nullVar = null
+
+console.log( number)
+    console.log( myName)
+    console.log( bigNumber)
+    console.log( caca)
+    console.log( nullVar)
+    console.log( noDefined)
+
+if (caca == true){
+    console.log(number + 1)
+    console.log(typeof myName)
+    console.log(typeof bigNumber)
+    console.log(typeof caca)
+    console.log(typeof nullVar)
+    console.log(typeof noDefined)
+} 
---
+++ operadores.js
@@ -0,0 +1,39 @@
+//operadores 
+
+//operadores arimteticos
+
+let a = 5
+
+let b = 10
+
+console.log(a+b)//suma
+console.log(a - b)//resta
+console.log(a * b)//mult
+console.log(a / b)//div
+
+console.log(a % b)//mod
+console.log(a ** b)//Exponente
+
+a++
+console.log(a)//Incremento
+
+b--
+console.log(b)//Decremento
+
+// Operadores de asignación
+
+let myVar = 2 
+console.log(myVar)
+myVar += 2
+console.log(myVar)
+myVar /= 2
+console.log(myVar)
+
+// Operadores de comparación
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a === a)
