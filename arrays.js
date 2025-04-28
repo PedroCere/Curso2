23cf9d6557daf20e1a2dd7ba4ee809d02b0a40f8
Pedro Mariano Cereghetti <pedrocerega@gmail.com>
2025-04-28 18:11:09 -0300

    Arrays_and_Sets

---
+++ arrays.js
@@ -0,0 +1,44 @@
+// array
+
+// declaración
+let myArray = []
+let myArray2 = new Array()
+
+console.log(myArray)
+console.log(myArray2)
+
+// inicialización
+
+myArray = [1]
+myArray2 = new Array(1)
+
+console.log(myArray)
+console.log(myArray2)
+
+myArray[1,2,3,"hola"]
+
+console.log(myArray)
+
+myArray[2]="hola"
+
+console.log(myArray)
+
+myArray.push("Brais")
+myArray.push("caca")
+
+console.log(myArray)
+
+myArray.pop
+console.log(myArray)
+myArray = [1, 2, 3, "hola"]
+myArray[2] = "adiós"
+console.log(myArray)
+
+myArray.push("Brais", "caca")
+console.log(myArray)
+
+myArray.pop()
+myArray.pop()
+myArray.pop()
+myArray.pop()
+console.log(myArray)
